32309116416151fc6a32b4a7059fd4ba501fe9bf
Serhat Karaman <90270116+MasterofN0ne@users.noreply.github.com>
2022-06-01 22:19:42 +0300

    Initial commit

---
+++ content.js
@@ -0,0 +1,6 @@
+var currentUrl = window.location.href;
+
+if (currentUrl.indexOf("twitter") !== -1){
+    window.location.replace("https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley");
+}
+
