fd2e6403f6b9757d5e4667695d6e6fce0313d2fe
Serhat Karaman <90270116+MasterofN0ne@users.noreply.github.com>
2022-06-02 10:12:26 +0300

    added a alert

--- content.js
+++ content.js
@@ -1,6 +1,7 @@
 var currentUrl = window.location.href;
 
 if (currentUrl.indexOf("twitter") !== -1){
+    alert("NO TWITTER IDIOT");
     window.location.replace("https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley");
 }
 
